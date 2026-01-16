6c11cac90d1274daa9d39f28954f653f742775f6
NerArth <notorp@gmail.com>
2026-01-16 23:03:04 +0000

    add blank calculation logic script with initial comments for intents

---
+++ calculator.py
@@ -0,0 +1,5 @@
+# This will be script where we do all calculations-related logic
+# for the app, making this the primary backend script.
+
+# TODO: Start writing relevant code;
+# - We'll need to import the data from `Satisfactory Recipes - Compiled by FE6515.csv`
