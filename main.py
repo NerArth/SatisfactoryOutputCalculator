36147eb8cfed0c6288bb30e13b8fceae57c1ed1f
Reilers853 <danilo.braga1707@gmail.com>
2026-01-16 15:32:29 -0300

    the app starts at the center

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import tkinter as tk
+from screeninfo import get_monitors
+import math
+
+
 root = tk.Tk()
+root.title("Output Calculator")
+root.configure(background="#42340c")
+root.minsize(600,400)
+
+screenx = get_monitors()[0].width
+screeny = get_monitors()[0].height
+
+root.geometry(f"{math.floor(screenx*0.33)}x{math.floor(screeny*0.33)}+{math.floor(screenx*0.33)}+{math.floor(screeny*0.33)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 root.mainloop()
