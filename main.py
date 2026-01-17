2830a201a8de4909d4881f48839f8dfff809c19a
Reilers853 <danilo.braga1707@gmail.com>
2026-01-17 08:51:05 -0300

    changed the title, removed by labels, put the label and input box on the top left

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+import tkinter.font as tkfont
 from screeninfo import get_monitors
 import math
 
 
 root = tk.Tk()
-root.title("Output Calculator")
+root.title("Satisfactory Output Calculator")
 root.configure(background="#42340c")
 root.minsize(900,700)
 
@@ -13,10 +14,16 @@ screeny = get_monitors()[0].height
 
 root.geometry(f"{math.floor(screenx*0.33)}x{math.floor(screeny*0.33)}+{math.floor(screenx*0.25)}+{math.floor(screeny*0.05)}")
 
-tk.Label(root, text="Satisfactory Output Calculator", font = ("Impact", 25), background = "#42340c", fg = "white").pack()
-tk.Label(root, text="By Dan and John", background = "#42340c", fg = "white").pack()
+f = tkfont.Font(family='Arial', size=18, weight='bold', underline=1)
+parameters = tk.Label(root, text="Parameters", font=f, background="#42340c", fg="#B4B4B4")
+parameters.place(relx=0.0, rely=0.0, anchor="nw")
 
-tk.Text(root, height = 1, width = 20, bg = "black", fg = "white").pack()
+desired_item = tk.Label(root, text="Desired item:", font=('Arial', 12), background="#42340c", fg="white")
+desired_item.place(relx=0.0, rely=0.06, anchor="nw")
+
+input1 = tk.Text(root, height = 1, width = 20, bg = "black", fg = "white")
+input1.place(relx=0.12, rely=0.06, anchor="nw")
+
 
 
 
