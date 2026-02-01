33953d1781017142634346fae916d56862e5defc
NerArth <notorp@gmail.com>
2026-02-01 15:57:20 +0000

    added basic calculator logic with `getItemNames` to return names from data csv

--- calculator.py
+++ calculator.py
@@ -3,3 +3,21 @@
 
 # TODO: Start writing relevant code;
 # - We'll need to import the data from `Satisfactory Recipes - Compiled by FE6515.csv`
+
+import pandas as pd
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def calculate(self, desired_item, desired_outputrate):
+        pass
+    
+    def getItemNames(self):
+        _data = pd.read_csv("Satisfactory Recipes - Compiled by FE6515.csv")
+        _ = []
+        for _tier in _data["LOCK"]:
+            if _tier == "FICSMAS":
+                continue
+            _.append(_data["NAME"])
+        return _ 
